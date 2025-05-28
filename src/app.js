c4104a74c05691b0f6bc06d9f4608ade3cfae801
Sam Dwyer <sam.dwyer@nine.com.au>
2025-05-28 11:14:01 +1000

    Errors sent as vanilla instrumentation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const { trace, SpanStatusCode } = require('@opentelemetry/api');
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/api', async (req, res) => {
-    const span = trace.getActiveSpan();
     const randomNum = Math.random();
     if (randomNum < 0.9) {
         res.status(200).send(`Success`);
@@ -19,11 +16,6 @@ app.get('/api', async (req, res) => {
             { status: 503, message: 'Downstream Phoenix failure' },
         ];
         const randomError = errorTypes[Math.floor(Math.random() * errorTypes.length)];
-        if (span) {
-            //{resource.service.name="error-testing-app" && span.http.status_code >=500} | rate() by(span.http.status_code)
-            span.setStatus({ code: SpanStatusCode.ERROR, message: randomError.message });
-            span.recordException({ code: randomError.status, message: randomError.message });
-        }
         res.status(randomError.status).send(randomError.message);
     }
 });
